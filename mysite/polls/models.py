6183fdfcd70ac3db815361e7a51e56c2e8b0882d
Panos <panoslin@foxmail.com>
2020-12-21 12:26:47 +0800

    modify: customize admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 import datetime
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, verbose_name='问题')
+    pub_date = models.DateTimeField('发布日期')
 
     def __str__(self):
         return self.question_text
@@ -12,6 +12,10 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = '最近发布?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
